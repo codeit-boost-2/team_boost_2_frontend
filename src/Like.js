e7bf997fe5c99da2823f7fe7c120f9e3b98e23cb
허성지 <co4331@naver.com>
2024-08-12 01:03:01 +0900

    Like.js 완성

---
+++ src/Like.js
@@ -0,0 +1,23 @@
+import LikeIcon from "./assets/LikeIcon.jpg"
+
+const style={
+    display: "flex",
+    alignItems: "center",
+    backgroundColor: "#ffffff",
+    padding: "12px 40px",
+    borderRadius: "5px",
+    fontFamily: "Spoqa Han Sans Neo, Sans-Serif",
+    fontWeight: "500",
+    fontSize: "15px",
+};
+
+function Like(){
+    return(
+        <button style={style}>
+            <img style={{padding : "4px"}} alt="icon" src={LikeIcon} />
+            공감 보내기
+        </button>
+    );
+}
+
+export default Like;

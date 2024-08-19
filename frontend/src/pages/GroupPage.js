e1cb35ecb6d4d219ea227742935427aff21be98f
lulu <annieroh@ewha.ac.kr>
2024-08-19 16:18:37 +0900

    frontend 파일 통합

--- frontend/src/pages/GroupPage.js
+++ frontend/src/pages/GroupPage.js
@@ -1,48 +1,58 @@
-import Card from '../components/CardMemory.js';
-import Info from '../components/Info.js';
-import Gnb from '../components/Gnb.js';
-import Button from '../components/Button.js';
-import Tab from '../components/oldTab.js';
-import Search from '../components/Search.js';
-const style={
-    margin:'20px',
-    display: 'flex',
-    fontFamily: 'Spoqa Han Sans Neo, Sans-Serif',
-    fontWeight: '600',
-    justifyContent:'center',
-    alignItems:'center',
-
+import Card from "../components/CardMemory.js";
+import Info from "../components/Info.js";
+import Gnb from "../components/Gnb.js";
+import Button from "../components/Button.js";
+import Tab from "../components/oldTab.js";
+import Search from "../components/Search.js";
+const style = {
+  margin: "20px",
+  display: "flex",
+  fontFamily: "Spoqa Han Sans Neo, Sans-Serif",
+  fontWeight: "600",
+  justifyContent: "center",
+  alignItems: "center",
 };
 
-const feedstyle={
-    display:'grid',
-    gridTemplateColumns:'repeat(4, 1fr)',
-    gridAutoRows: '561px',
-    margin:'12px',
-    gap:'10px'
+const feedstyle = {
+  display: "grid",
+  gridTemplateColumns: "repeat(4, 1fr)",
+  gridAutoRows: "561px",
+  margin: "12px",
+  gap: "10px",
 };
-function GroupPage(){
-    return(
+function GroupPage() {
+  return (
     <>
-        <Gnb />
-        <Info />
-        <hr />  
-        <div style={style}>
-            <div style={{flexGrow:'1', display: 'flex', justifyContent:'center', alignItems:'center'}}>추억목록</div>
-            <Button />
+      <Gnb />
+      <Info />
+      <hr />
+      <div style={style}>
+        <div
+          style={{
+            flexGrow: "1",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          추억목록
         </div>
-        <div style={{display:"flex", marginBottom: '40px'}}><Tab /><Search /></div>
-        <div className='feed' style={feedstyle}>
+        <Button />
+      </div>
+      <div style={{ display: "flex", marginBottom: "40px" }}>
+        <Tab />
+        <Search />
+      </div>
+      <div className="feed" style={feedstyle}>
         <Card />
         <Card />
         <Card />
         <Card />
         <Card />
         <Card />
-        </div>
-        
+      </div>
     </>
-    );
+  );
 }
 
 export default GroupPage;
--- frontend/src/pages/Makegroup_page.js
+++ frontend/src/pages/Makegroup_page.js
@@ -1,11 +1,18 @@
 import { Link } from "react-router-dom";
 
 function MakeGroupPage() {
+  const titleStyle = {
+    marginTop: "200px",
+    display: "flex",
+    justifyContent: "center",
+  };
+
   return (
-    <div>
+    <>
+      <h2 style={titleStyle}>그룹 만들기</h2>
       <title>make group</title>
       <div>make group</div>
-    </div>
+    </>
   );
 }
 export default MakeGroupPage;

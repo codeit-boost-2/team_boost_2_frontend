869557ff6741fccb91319855a9e6ca75a881a11d
gjtjdwl <149356825+gjtjdwl@users.noreply.github.com>
2024-08-19 11:38:22 +0900

    Delete src/index.js
    중복 제거

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import GroupPage from './pages/GroupPage.js';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<GroupPage />);
-

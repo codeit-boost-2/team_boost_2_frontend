ee794925124cd2175075261105e7176e992a1ad8
seonghwan <wkrdmsxowkd@naver.com>
2024-08-20 13:12:06 +0900

    추억 상세 정보 조회, 추억 공개 여부 확인 update

--- backend/router/memoryRouter.js
+++ backend/router/memoryRouter.js
@@ -8,10 +8,29 @@ const prisma = new PrismaClient();
 const memoryRouter = express.Router();
 memoryRouter.use(express.json());
 
-// 전체 추억 조회
-memoryRouter.get('/', asyncHandler(async (req, res) => {
-  const memories = await prisma.memory.findMany();
-  res.send(memories);
-}));
+memoryRouter.route('/:id')
+  // 추억 상세 정보 조회
+  .get(asyncHandler(async (req, res) => {
+    const { id } = req.params
+    const memory = await prisma.memory.findUniqueOrThrow({
+      where: { id },
+    });
+
+    res.status(200).send(memory);
+  }));
+
+memoryRouter.route('/:id/isPublic')
+  .get(asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    const memory = prisma.memory.findUniqueOrThrow({
+      where: { id },
+      select: {
+        id: true,
+        isPublic: true,
+      },
+    });
+
+    res.status(200).send(memory)
+  }));
 
 export default memoryRouter;

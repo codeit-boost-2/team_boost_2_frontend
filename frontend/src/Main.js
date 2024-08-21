3ea4d3ab39b846ecf541e6e90bb97e7436701b86
허성지 <co4331@naver.com>
2024-08-21 23:55:08 +0900

    feat: 비공개 추억 클릭시 권한 확인 페이지로 이동 rename: CardMemory 언더바 형식으로 수정

--- frontend/src/Main.js
+++ frontend/src/Main.js
@@ -3,6 +3,7 @@ import App from "./components/App";
 import GroupListPage from "./pages/Grouplist_page";
 import MakeGroupPage from "./pages/Makegroup_page";
 import GroupPage from "./pages/Group_page";
+import AuthenticationPage from "./pages/Authentication_page";
 //나머지 페이지도 import
 
 function Main() {
@@ -13,6 +14,7 @@ function Main() {
           <Route index element={<GroupListPage />} />
           <Route path="/makeGroup" element={<MakeGroupPage />} />
           <Route path="/GroupPage" element={<GroupPage />} />
+          <Route path="/AutPage" element={<AuthenticationPage />} />
           {/* Route path="다른페이지" element={<다른페이지 />} */}
         </Route>
       </Routes>
--- frontend/src/pages/Authentication_page.js
+++ frontend/src/pages/Authentication_page.js
@@ -1,9 +1,27 @@
+
+const pageStyle = {
+    marginTop: "300px",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column",
+    fontFamily: "Spoqa Han Sans Neo, Sans-Serif",
+    fontWeight: "500",
+    fontSize: "15px",
+};
+
 function AuthenticationPage(){
     return(
         <>
-        <div>비공개 추억</div>
-        <div>비공개 추억에 접근하기 위해 권한 확인이 필요합니다.</div>
-        
+        <div style={pageStyle}>
+            <div style={{margin:"20px", fontWeight:"700", fontSize:"30px"}}>비공개 추억</div>
+            <div style={{marginBottom:"20px", fontWeight:"300", fontSize:"17px"}}>비공개 추억에 접근하기 위해 권한 확인이 필요합니다.</div>
+            <form>
+                비밀번호
+            </form>
+        </div>
         </>
     )
 }
+
+export default AuthenticationPage;

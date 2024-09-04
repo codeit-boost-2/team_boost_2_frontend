b626e34b07aed14393e8e8148b8748a20ac67b4b
seonghwan <wkrdmsxowkd@naver.com>
2024-09-04 20:19:23 +0900

    cors 에러 관련 수정

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,17 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
 import { PrismaClient } from '@prisma/client';
+import http from 'http';
+import cors from 'cors';
 
 const prisma = new PrismaClient();
 const app = express();
 app.use(express.json());
+app.use(cors({
+    origin: "http://localhost:3000", // 접근 권한을 부여하는 도메인
+    credentials: true, // 응답 헤더에 Access-Control-Allow-Credentials 추가
+    optionsSuccessStatus: 200, // 응답 상태 200으로 설정
+}));
 
 import groupRouter from './router/groupRouter.js';
 import memoryRouter from './router/memoryRouter.js';

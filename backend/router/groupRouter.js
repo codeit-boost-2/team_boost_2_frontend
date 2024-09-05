c8931b13f5923c71ca5aafcf34a707e360c1c2b3
jungh150 <jungh150@naver.com>
2024-09-05 11:23:36 +0900

    디버깅 코드 추가

--- backend/router/groupRouter.js
+++ backend/router/groupRouter.js
@@ -73,8 +73,11 @@ groupRouter.route('/:page/:pageSize')
       form.append(`data[${index}][createdAt]`, group.createdAt.toISOString());
       form.append(`data[${index}][introduction]`, group.description);
 
-      // Add the image file
-      const imagePath = path.join('/home/ec2-user/team_boost_2/images', group.image);
+      const imagePath = path.join(`${process.env.IMAGE_DIR}`, group.image);
+      console.log(`${process.env.IMAGE_DIR}`);
+      console.log(group.image);
+      console.log(imagePath);
+
       if (fs.existsSync(imagePath)) {
         form.append(`data[${index}][image]`, fs.createReadStream(imagePath));
       } else {

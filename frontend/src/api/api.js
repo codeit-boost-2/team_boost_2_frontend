42568a3809548b6e745587da54eac8433e874343
lulu010223 <annieroh01@gmail.com>
2024-09-06 12:24:58 +0900

    Update api.js(추억 상세 페이지 불러오기, 공감보내기 상태 업데이트 추가)

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -47,6 +47,23 @@ export async function getMemories({
   return body;
 }
 
+// 추억 상세 페이지 불러오기
+export async function getDataById(id) {
+  const response = await fetch(`${BASE_URL}/GroupPage/${id}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error("데이터를 가져오는데 실패했습니다.");
+  }
+
+  const body = await response.json();
+  return body;
+}
+
 // 추억 상세 페이지 수정
 export async function editMemory(id, updatedItem) {
   const response = await fetch(`${BASE_URL}/GroupPage/${id}`, {
@@ -117,4 +134,29 @@ export async function deleteReply(id, password) {
 
   const body = await response.json();
   return body;
+}
+
+// 공감보내기 상태 업데이트
+export function updateLikeCount(id) {
+  return fetch(`${BASE_URL}/GroupPage/${id}/like`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      action: "like",
+    }),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("좋아요 상태 업데이트에 실패했습니다.");
+      }
+      return response.json();
+    })
+    .then((result) => {
+      return result;
+    })
+    .catch((error) => {
+      console.error("서버 요청 중 오류 발생:", error);
+    });
 }

1446e376be695b5cf519537f6e19525a0de99791
jungh150 <jungh150@naver.com>
2024-09-07 15:07:51 +0900

    프론트 배포로 url 수정

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const prisma = new PrismaClient();
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:3000", // 접근 권한을 부여하는 도메인
+    origin: "http://teamboost2.zogakzip.s3-website.eu-north-1.amazonaws.com", // 접근 권한을 부여하는 도메인
     credentials: true, // 응답 헤더에 Access-Control-Allow-Credentials 추가
     optionsSuccessStatus: 200, // 응답 상태 200으로 설정
 }));
